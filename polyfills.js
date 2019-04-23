07ca72095cdbbfbc09128f0e7e366c795c06d5a9
Silv <silvan.otten@windesheim.nl>
2019-04-23 20:30:37 +0200

    better active linking, chicken images, added 404 & bare bone contact

--- polyfills.js
+++ polyfills.js
@@ -5784,8 +5784,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\eek_d\Desktop\Live\silvanotten.github.io\src\polyfills.ts */"./src/polyfills.ts");
-module.exports = __webpack_require__(/*! C:\Users\eek_d\Desktop\Live\silvanotten.github.io\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
+__webpack_require__(/*! c:\Users\eek_d\Desktop\Live\silvanotten.github.io\src\polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! c:\Users\eek_d\Desktop\Live\silvanotten.github.io\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
 
 
 /***/ })
