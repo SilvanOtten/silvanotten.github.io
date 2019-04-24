4eefc71e90bb3f91c8a949e5ec297f8416da37e6
Silv <silvan.otten@windesheim.nl>
2019-04-24 09:31:06 +0200

    Google analytics route tracking

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
 // Angular
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+
+// declare ga as a function to set and sent the events
+// tslint:disable-next-line: ban-types
+declare let ga: Function;
 
 @Component({
   selector: 'app-root',
@@ -8,4 +13,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public title = 'Silvan Otten';
+
+  constructor(public router: Router) {
+    // subscribe to router events and send page views to Google Analytics
+    this.router.events.subscribe(event => {
+
+      if (event instanceof NavigationEnd) {
+        ga('set', 'page', event.urlAfterRedirects);
+        ga('send', 'pageview');
+      }
+    });
+  }
+
 }
